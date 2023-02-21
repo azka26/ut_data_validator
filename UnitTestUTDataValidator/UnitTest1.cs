9ff2314a07e2a10b4bc840941db605ee4cdbf684
Rizal Rosa <rosa.andika@live.com>
2023-02-21 12:39:39 +0700

    Perbaikan unit test

--- UnitTestUTDataValidator/UnitTest1.cs
+++ UnitTestUTDataValidator/UnitTest1.cs
@@ -6,6 +6,7 @@ using System.IO;
 using UTDataValidator;
 using System.Data.Common;
 using System.Data.SqlClient;
+using Newtonsoft.Json;
 using OfficeOpenXml;
 
 namespace UnitTestProject1
@@ -14,40 +15,29 @@ namespace UnitTestProject1
     {
         private string ConnectionString = "Data Source=localhost,1433;Initial Catalog=UnitTest;User ID=sa;Password=Strong@Password;";
         [Test()]
-        public void ReadExcelData_Test()
+        public void ExcelValidator_Test()
         {
-            var sqlDelete = "DELETE FROM SampleTable";
-            var dataList = new List<string>()
-            {
-                $@" SET IDENTITY_INSERT dbo.SampleTable ON;  
-                    INSERT INTO SampleTable (Id, Name, CreatedDate, CreatedTime, CreatedDateTime, ValueInt, ValueDouble) 
-                    VALUES ('0', 'Rizal', '2022-01-01', '00:00:00', '2022-01-01 00:00:00.000','20','20.1');
-                    SET IDENTITY_INSERT dbo.SampleTable OFF;
-                ",
-                $@" SET IDENTITY_INSERT dbo.SampleTable ON;
-                    INSERT INTO SampleTable (Id, Name, CreatedDate, CreatedTime, CreatedDateTime, ValueInt, ValueDouble) 
-                    VALUES ('1', 'Rosa', '2022-01-02', '00:01:00', '2022-01-02 00:01:00.000','21','20.2');
-                    SET IDENTITY_INSERT dbo.SampleTable OFF;
-                ",
-                $@" SET IDENTITY_INSERT dbo.SampleTable ON;
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "excel", "sample-tipe-data.xlsx");
+            ExcelValidator excelValidator = new ExcelValidator(new Assertion(), path, "init", "expected", this);
+
+            ExecuteNonQuery($@" SET IDENTITY_INSERT dbo.SampleTable ON;
                     INSERT INTO SampleTable (Id, Name, CreatedDate, CreatedTime, CreatedDateTime, ValueInt, ValueDouble) 
                     VALUES ('2', 'Andika', '2022-01-03', '00:02:00', '2022-01-03 00:02:00.000','22','20.3');
                     SET IDENTITY_INSERT dbo.SampleTable OFF;
-                "
-            };
-
-            ExecuteNonQuery(sqlDelete);
-            foreach (var sqlInsert in dataList)
-            {
-                ExecuteNonQuery(sqlInsert);
-            }
-
-            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "excel", "sample-tipe-data.xlsx");
-            ExcelValidator excelValidator = new ExcelValidator(new Assertion(), path, "init", "init", this);
+                ");
+            
             excelValidator.Validate();
         }
 
-        private int ExecuteNonQuery(string sql)
+        [Test]
+        public void JsonValidator_Test()
+        {
+            JsonDataValidator jsonDataValidator = new JsonDataValidator(new Assertion(),
+                JsonConvert.SerializeObject(new { Id = "1", Name = "Rizal" }));
+            jsonDataValidator.ValidateData(JsonConvert.SerializeObject(new { Id = "1", Name = "Rizal" }));
+        }
+
+        private int ExecuteNonQuery(string sql, List<SqlParameter> parameters = null)
         {
             using (var sqlConnection = new SqlConnection(ConnectionString))
             {
@@ -56,6 +46,12 @@ namespace UnitTestProject1
                 {
                     sqlCommand.CommandType = CommandType.Text;
                     sqlCommand.CommandText = sql;
+
+                    if (parameters != null)
+                    {
+                        sqlCommand.Parameters.AddRange(parameters.ToArray());
+                    }
+                    
                     return sqlCommand.ExecuteNonQuery();
                 }
             }
@@ -83,27 +79,34 @@ namespace UnitTestProject1
 
         public DataTable ReadTable(ExcelDataDefinition excelDataDefinition)
         {
-            DataTable dataTable = new DataTable();
-            using (var sqlConnection = new SqlConnection(ConnectionString))
-            {
-                sqlConnection.Open();
-                using (var sqlCommand = sqlConnection.CreateCommand())
-                {
-                    sqlCommand.CommandType = CommandType.Text;
-                    sqlCommand.CommandText = "SELECT * FROM " + excelDataDefinition.Table;
-                    using (var reader = sqlCommand.ExecuteReader())
-                    {
-                        dataTable.Load(reader);
-                    }
-                }
-            }
-
-            return dataTable;
+            return ExecuteQuery($"SELECT * FROM {excelDataDefinition.Table}");
         }
 
         public void InitData(IEnumerable<ExcelDataDefinition> excelDataDefinition)
         {
-            
+            foreach (var definition in excelDataDefinition)
+            {
+                ExecuteNonQuery($"DELETE FROM {definition.Table}");
+                foreach (DataRow dataRow in definition.Data.Rows)
+                {
+                    var fields = new List<string>();
+                    var fieldParameters = new List<string>();
+                    var parameters = new List<SqlParameter>();
+                    foreach (DataColumn column in definition.Data.Columns)
+                    {
+                        fields.Add(column.ColumnName);
+                        fieldParameters.Add($"@{column.ColumnName}");
+                        parameters.Add(new SqlParameter($"@{column.ColumnName}", dataRow[column.ColumnName]));
+                    }
+
+                    var sql =
+                        $@"     SET IDENTITY_INSERT {definition.Table} ON;
+                                INSERT INTO {definition.Table} ({string.Join(", ", fields)}) VALUES ({string.Join(", ", @fieldParameters)});
+                                SET IDENTITY_INSERT {definition.Table} OFF;
+                        ";
+                    ExecuteNonQuery(sql, parameters);
+                }
+            }
         }
 
         public void ExecuteAction(TestAction testAction)
